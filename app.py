373fb5335fa1f21bfacb422f2190dc01e8679d4a
Miguel Becerra <miguelbecerra96@outlook.com>
2024-06-23 11:48:52 -0600

    charts

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ scatter_button = st.button("Creación de Grafica de Dispersion")
 if hist_button:
     st.write(
         "Creación de un histograma para el conjunto de datos de anuncios de ventas de coches")
-    fig1 = st.hist(car_data, x="odometer")
+    fig1 = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
 if scatter_button:
     st.write("creación de grafica de dispersion para el conjunto de datos de anuncios de ventas de coches")
-    fig2 = st.scatter(car_data, x="odometer")
+    fig2 = px.scatter(car_data, x="odometer")
     st.plotly_chart(fig2, use_container_widht=True)
 
 check_hist = st.checkbox("Realizar Histograma")
@@ -24,10 +24,10 @@ check_scatter = st.checkbox("Realizar Grafica de dispersión")
 if check_hist:
     st.write(
         "Creación de Histograma para el conjunto de datos de anuncios de ventas de coches")
-    fig3 = st.hist(car_data, x="odometer")
+    fig3 = px.hist(car_data, x="odometer")
     st.plotly_chart(fig3, use_container_width=True)
 
 if check_scatter:
     st.write("Cración de grafica de dispersion para el conjunto de datos de anuncios de ventas de coches")
-    fig4 = st.scatter(car_data, x="odometer")
+    fig4 = px.scatter(car_data, x="odometer")
     st.plotly_chart(fig4, use_container_widht=True)
