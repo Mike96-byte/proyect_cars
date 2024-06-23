efd1a4df907ab296be07ca87ae13b769401deebf
Miguel Becerra <miguelbecerra96@outlook.com>
2024-06-23 11:43:33 -0600

    last changes in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import plotly_express as px
 
 car_data = pd.read_csv('vehicles_us.csv')
 
@@ -10,10 +10,24 @@ scatter_button = st.button("Creación de Grafica de Dispersion")
 if hist_button:
     st.write(
         "Creación de un histograma para el conjunto de datos de anuncios de ventas de coches")
-    fig = st.hist(car_data, x="odometer")
+    fig1 = st.hist(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
 if scatter_button:
     st.write("creación de grafica de dispersion para el conjunto de datos de anuncios de ventas de coches")
     fig2 = st.scatter(car_data, x="odometer")
     st.plotly_chart(fig2, use_container_widht=True)
+
+check_hist = st.checkbox("Realizar Histograma")
+check_scatter = st.checkbox("Realizar Grafica de dispersión")
+
+if check_hist:
+    st.write(
+        "Creación de Histograma para el conjunto de datos de anuncios de ventas de coches")
+    fig3 = st.hist(car_data, x="odometer")
+    st.plotly_chart(fig3, use_container_width=True)
+
+if check_scatter:
+    st.write("Cración de grafica de dispersion para el conjunto de datos de anuncios de ventas de coches")
+    fig4 = st.scatter(car_data, x="odometer")
+    st.plotly_chart(fig4, use_container_widht=True)
