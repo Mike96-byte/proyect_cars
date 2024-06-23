c06d65c5bbd3191016173701297069fc4172cbca
Miguel Becerra <miguelbecerra96@outlook.com>
2024-06-23 11:25:36 -0600

    cambios en app.py

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ import plotly.express as px
 car_data = pd.read_csv('vehicles_us.csv')
 
 hist_button = st.button("Creación de Histograma")
+scatter_button = st.button("Creación de Grafica de Dispersion")
+
+if hist_button:
+    st.write(
+        "Creación de un histograma para el conjunto de datos de anuncios de ventas de coches")
+    fig = st.hist(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+    st.write("creación de grafica de dispersion para el conjunto de datos de anuncios de ventas de coches")
+    fig2 = st.scatter(car_data, x="odometer")
+    st.plotly_chart(fig2, use_container_widht=True)
